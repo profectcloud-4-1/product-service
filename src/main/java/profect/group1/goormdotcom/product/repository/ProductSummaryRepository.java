631ddd68ed698667298bab550b599f7d43658041
ThinkKat <jwy06048@gmail.com>
2025-11-15 21:32:28 +0900

    :wrench: [chore] Remove unnecessary files

--- src/main/java/profect/group1/goormdotcom/product/repository/ProductSummaryRepository.java
+++
@@ -1,9 +0,0 @@
-package profect.group1.goormdotcom.product.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import profect.group1.goormdotcom.product.repository.entity.ProductSummaryEntity;
-
-import java.util.UUID;
-
-public interface ProductSummaryRepository extends CrudRepository<ProductSummaryEntity, UUID> {
-}
