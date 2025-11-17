90a1a69aced56bcc26c3459039a3d9b0fa95d3d2
ThinkKat <jwy06048@gmail.com>
2025-11-17 15:52:13 +0900

    :wrench: [chore] remove unnecessary library

--- src/main/java/profect/group1/goormdotcom/common/config/CacheConfig.java
+++ src/main/java/profect/group1/goormdotcom/common/config/CacheConfig.java
@@ -1,14 +1,11 @@
 package profect.group1.goormdotcom.common.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
