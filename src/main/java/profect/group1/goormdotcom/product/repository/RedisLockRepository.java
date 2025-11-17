3e8ea20a9df858f7a3090c60cef1a746b28ee73a
ThinkKat <jwy06048@gmail.com>
2025-11-18 01:48:15 +0900

    :sparkles: [feature] 장애 대응 - 캐시스탬피드 방지 로직을 위한 분산락

--- src/main/java/profect/group1/goormdotcom/product/repository/RedisLockRepository.java
+++ src/main/java/profect/group1/goormdotcom/product/repository/RedisLockRepository.java
@@ -1,4 +1,26 @@
 package profect.group1.goormdotcom.product.repository;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
 public class RedisLockRepository {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public Boolean lock(String lockKey) {
+        return redisTemplate
+                .opsForValue()
+                .setIfAbsent(lockKey, "lock", Duration.ofMillis(3000));
+    }
+
+    public Boolean unlock(String lockKey) {
+        return redisTemplate.delete(lockKey);
+    }
+
 }
