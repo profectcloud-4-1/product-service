ac8c886aa20430f5e1c5d6a4cf7f5da745c8df56
ThinkKat <jwy06048@gmail.com>
2025-12-06 10:57:58 +0900

    :hammer: [fix] modify serialization/deserialization and topics and consumer-group

--- src/main/java/profect/group1/goormdotcom/kafka/config/KafkaConsumerConfig.java
+++ src/main/java/profect/group1/goormdotcom/kafka/config/KafkaConsumerConfig.java
@@ -1,7 +1,6 @@
-package profect.group1.goormdotcom.kafka;
+package profect.group1.goormdotcom.kafka.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,11 +19,14 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Value("${spring.kafka.sasl.username:}")
+    @Value("${spring.kafka.sasl.username}")
     private String saslUsername;
 
-    @Value("${spring.kafka.sasl.password:}")
+    @Value("${spring.kafka.sasl.password}")
     private String saslPassword;
+
+    @Value("${spring.kafka.local}")
+    private boolean local;
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
@@ -35,10 +38,10 @@ public class KafkaConsumerConfig {
 
         // 보안설정 (MSK 사용 시 필요)
         // 로컬로 할 때는 spring.kafka.sasl.username와 spring.kafka.sasl.password를 설정하지 않음 (.env에서 설정안하면 됨.)
-        if (saslUsername != null && saslPassword != null) {
+        if (!local) {
             props.put("security.protocol", "SASL_SSL");
             props.put("sasl.mechanism", "SCRAM-SHA-512");
-            
+
             // SCRAM username과 password를 동적으로 주입
             String jaasConfig = String.format(
                 "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";",
@@ -56,7 +59,6 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
-        factory.setConcurrency(1);
         return factory;
     }
 }
